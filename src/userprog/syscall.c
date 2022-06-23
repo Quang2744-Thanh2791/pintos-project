f3a1a429a12536144e19f2995da644dd8d516534
quang <quangtraan1604@gmail.com>
2022-06-23 09:10:01 +0700

    click here

---
+++ src/userprog/syscall.c
@@ -0,0 +1,39 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "process.h"
+
+static void syscall_handler (struct intr_frame *);
+
+void
+syscall_init (void) 
+{
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  printf ("system call!\n");
+  thread_exit ();
+}
+//  @@@ 
+void sys_exit(int status) {
+  printf("%s: exit(%d)\n", thread_current()->name, status);
+
+  // The process exits.
+  // wake up the parent process (if it was sleeping) using semaphore,
+  // and pass the return code.
+  struct process_control_block *pcb = thread_current()->pcb;
+  if(pcb != NULL) {
+    pcb->exitcode = status;
+  }
+  else {
+    // pcb == NULL probably means that previously
+    // page allocation has failed in process_execute()
+  }
+
+  thread_exit();
+}
